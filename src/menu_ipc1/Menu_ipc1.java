36d9e6b48558d75f664c3f88a51a4b3c7ed08c11
Aragon Perez <Aragon Perez@Selvin>
2018-02-19 17:08:30 -0600

    Estoy Trabajando en mi tarea

--- src/menu_ipc1/Menu_ipc1.java
+++ src/menu_ipc1/Menu_ipc1.java
@@ -18,35 +18,62 @@ public class Menu_ipc1 {
         int j=1;
         int a,b,c, mayor = 0, medio=0, menor=0,error=0 ;
         int contdigitos=1;
-        Scanner Ing_teclado = new Scanner(System.in);
+        limpiar limpio=new limpiar();
+        int contador_principal=1; 
+        while(contador_principal>=1){
+            contador_principal++;
+            Scanner Ing_teclado = new Scanner(System.in);
         System.out.println("[IPC1]Tarea3_201701133");
         System.out.println("  ");
         System.out.println("1. Usuarios");
         System.out.println("2. Contador de Digitos");
         System.out.println("3. tres numeros de mayor a menor");
         System.out.println("4. Calcular Promedio");
+        System.out.println("5. Salir");
         int opcion= Ing_teclado.nextInt();
         
         switch (opcion){
             case 1:
-                System.out.println("Ingrese Usuario");
+                int contador_caso1=1;
+                while(contador_caso1==1){
+                    System.out.println("1. Ingresar Usuarios");
+                    System.out.println("2. Mostrar Usuarios Ascendente");
+                    System.out.println("3. Mostrar Usuarios Desendente");
+                    System.out.println("4. Menu Principal");
+                    int submenu2=Ing_teclado.nextInt();
+                    switch(submenu2){
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        default:
+                    }
+                }
+                
                 break;
                 
                 
-                
             case 2:
-                System.out.println("1. Ingresar Numero ");
-                System.out.println("2. Mostrar Numero de Digitos");
-                System.out.println("4. Menu Principal");
-                int submenu2=Ing_teclado.nextInt();
-                switch (submenu2){
-                    case 1:
-                        int numing= Ing_teclado.nextInt();
-                        if(numing==0){
+                limpio.limpiartodo();
+                int contador_caso2=1;
+                while(contador_caso2==1){
+                    System.out.println("1. Ingresar Numero ");
+                    System.out.println("2. Mostrar Numero de Digitos");
+                    System.out.println("4. Menu Principal");
+                    int submenu2=Ing_teclado.nextInt();
+                    switch (submenu2){
+                        case 1:
+                         System.out.println("Ingrese un numero");
+                         int numing= Ing_teclado.nextInt();
+                          if(numing==0){
                             System.out.println("Solo tiene 1 Digito");
-                        }else{
-                            if(numing>0 && numing<=100000 ){
-                                while(j>0){
+                            }else{
+                             if(numing>0 && numing<=100000 ){
+                                 while(j>0){
                                     numing=numing/10;
                         
                                     if(numing>=1 ){
@@ -64,18 +91,24 @@ public class Menu_ipc1 {
                         System.out.println("Es numero Tiene "+ contdigitos+ " Digitos");
                         break;
                     case 4:
+                        limpio.limpiartodo();
+                        contador_caso2++;
                         break;
                     default:
+                        limpio.limpiartodo();
                         System.out.println("Ingrese Correctamente la Opcion");
                 }
+                }
                 
-
-        
+ 
                 break;
             case 3:
                 //los valores de cada numero se representan en variables a(num 1), b (num2) y c(num3)
+                limpio.limpiartodo();
+                int contador_caso3=1;
+                while(contador_caso3==1){
                 System.out.println("1. Ingresar Numeros");
-                System.out.println("2. Mostrar Numero de Digitos");
+                System.out.println("2. Mostrar ordenados");
                 System.out.println("4. Menu Principal");
                 int submenu3=Ing_teclado.nextInt();
                 switch (submenu3){
@@ -135,14 +168,25 @@ public class Menu_ipc1 {
                         
                         break;
                     case 4:
+                        limpio.limpiartodo();
+                        contador_caso3++;
                         break;
+                    default:
+                        limpio.limpiartodo();
+                        System.out.println("Ingrese correctamente una opcion");
+                }
                 }
                 
                 break;
             case 4:
                 break;
+            case 5:
+                System.exit(0);
+                break;
             default:
-            
+                System.out.println("Ingrese la opcion correctamente");
+                contador_principal--;
         }
-    }   
+        }
+    } 
 }
